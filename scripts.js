2c839178fca17cae3ed81bafb6d635e3a1f438c9
Thomas E Fleming <thomasefleming@gmail.com>
2024-09-28 17:52:31 -0400

    commit

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
 var message = 'Hello, TypeScript!';
 console.log(message);
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.style.opacity = 0;
+    setTimeout(() => {
+        document.body.style.transition = "opacity 1.5s ease";
+        document.body.style.opacity = 1;
+    }, 100);
+});
