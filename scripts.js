c416b9cd9a9f5cbe839d960271536510279deeea
Thomas E Fleming <thomasefleming@gmail.com>
2024-09-28 17:58:40 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 var message = 'Hello, TypeScript!';
 console.log(message);
 
+//this creates a cool fade-in effect for your pages
 document.addEventListener("DOMContentLoaded", function() {
     document.body.style.opacity = 0;
     setTimeout(() => {
